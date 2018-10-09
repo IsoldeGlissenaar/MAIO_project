b1f790f905b89667d8808a7d8f958cb1b88f8af2
IsoldeGlissenaar <43541832+IsoldeGlissenaar@users.noreply.github.com>
2018-10-09 12:41:58 +0200

    cut daily  compare  with lufthavn

--- compareIsolde.py
+++ compareIsolde.py
@@ -12,14 +12,14 @@ import pandas as pd
 
 direc = ""
 
-T_ulve=np.load(direc+"Ulvebreen/THUTavg[C].npy")
-T_ulveday=np.load(direc+"Ulvebreen/Date.npy")
+T_ulve=np.load(direc+"avgUlvebreen/THUT2m[K].npy")
+T_ulveday=np.load(direc+"avgUlvebreen/THUT2m[K]day.npy")
 
-T_norden=np.load(direc+"Nordenskioldbreen/Tsurf.npy")
-T_nordenday=np.load(direc+"Nordenskioldbreen/tempnordendate.npy")
+T_norden=np.load(direc+"avgNordenskioldbreen/T2m.npy")
+T_nordenday=np.load(direc+"avgNordenskioldbreen/T2mday.npy")
 
-T_luft=np.load(direc+"Lufthavn/TA.npy")
-T_luftday=np.load(direc+"Lufthavn2/Date.npy")
+T_luft=np.load(direc+"avgLufthavn/TA.npy")
+T_luftday=np.load(direc+"avgLufthavn/TADay.npy")
 
 
 ''' This code cuts the data to the overlapping length'''
@@ -29,28 +29,32 @@ plt.plot(T_nordenday,T_norden,'.')
 plt.plot(T_luftday,T_luft,'.')
 plt.show()
 
-minday = np.min(T_ulveday)
+minday = T_ulveday[1]
 maxday = np.max(T_nordenday)
 
-delta = np.timedelta64(1,'30m')
-newminday = minday+2*delta
 
 minnord = np.where(T_nordenday == minday)[0][0]
-#minulve = np.where(T_ulveday   == minday)[0][0]
+maxnord  = np.where(T_nordenday   == maxday)[0][0]
+
+minulv  = np.where(T_ulveday   == minday)[0][0]
 maxulv  = np.where(T_ulveday   == maxday)[0][0]
 
-#minluft = np.where(T_luftday   == newminday)[0][0]
+minluft = np.where(T_luftday   == minday)[0][0]
+maxluft = np.where(T_luftday   == maxday)[0][0]
 
-T_ulve_c =  T_ulve[:maxulv]
-T_ulveday_c = T_ulveday[:maxulv]
 
-T_norden_c =  T_norden[minnord:]
-T_nordenday_c = T_nordenday[minnord:]
+T_ulve_c =  T_ulve[minulv:maxulv]
+T_ulveday_c = T_ulveday[minulv:maxulv]
 
-#T_luft_c = T_luft[minnord:maxulv]
+T_norden_c =  T_norden[minnord:maxnord]
+T_nordenday_c = T_nordenday[minnord:maxnord]
+
+T_luft_c = T_luft[minluft:maxluft]
+T_luftday_c = T_luftday[minluft:maxluft]
 
 plt.plot(T_nordenday_c,T_norden_c,'b.')
 plt.plot(T_ulveday_c,T_ulve_c,'g.')
+plt.plot(T_luftday_c,T_luft_c,'y.')
 plt.show()
 
 
@@ -60,12 +64,12 @@ This part of the code creates an array with values for the nordenskiold
 and the ulvebreen for every half hour, even when there were no measurements for both.
 When there is no value, this is masked.'''
 
-dates = np.arange('2015-08-22T17:00:00', '2016-12-03T17:00:00', dtype='datetime64[30m]')
+dates = np.arange('2015-08-26T00:00:00', '2016-07-11T00:00:00', dtype='datetime64[30m]')
 
 #miss_ulv = list(set(T_nordenday_c)-set(T_ulveday_c))
 #miss_nord = list(set(T_ulveday_c)-set(T_nordenday_c))
 
-T_c = np.empty([len(dates),2])
+T_c = np.empty([len(dates),3])
 T_c[:,:] = np.nan   
     
 
@@ -84,10 +88,19 @@ for i in range(0,len(dates)):
         T_c[i,1] = T_ulve_c[loc_ulv]
     except IndexError: # catch the error
         continue
+    
+for i in range(0,len(dates)):
+    try:
+    # the code that can cause the error
+        loc_luft  = np.where(T_luftday_c == dates[i])[0][0]
+        T_c[i,2] = T_luft_c[loc_luft]
+    except IndexError: # catch the error
+        continue
 
 
 plt.plot(dates, T_c[:,0],'b.') #nordenskioldbreen
 plt.plot(dates, T_c[:,1],'g.') #ulvebreen
+plt.plot(dates, T_c[:,2],'y.') #lufthavn
 plt.show()
 
 for i in range (0,len(dates)):
@@ -95,11 +108,13 @@ for i in range (0,len(dates)):
         if np.isnan(T_c[i,j]):
             T_c[i,0]=999
             T_c[i,1]=999
+            T_c[i,2]=999
             
 T_mask = np.ma.masked_equal(T_c, 999)
 
 plt.plot(dates, T_mask[:,0],'b.') #nordenskioldbreen
 plt.plot(dates, T_mask[:,1],'g.') #ulvebreen
+plt.plot(dates, T_mask[:,2],'y.') #lufthavn
 plt.show()
 
 #%%
@@ -118,14 +133,12 @@ plt.show()
 #  
     
 '''DM method'''
-#temp = T_c[:,1]
-#ulvNorm = temp.__truediv__(np.ma.MaskedArray.std(temp))
 ulvNorm = T_mask[:,1]/np.std(T_mask[:,1])
 nordNorm = T_mask[:,0]/np.std(T_mask[:,0])
 
 
-mat = np.matrix([ulvNorm,nordNorm])
-Covmatrix = np.cov(mat)/np.cov(mat)[1,1]
+mat = np.ma.array([ulvNorm,nordNorm])
+Covmatrix = np.ma.cov(mat)#/np.ma.cov(mat)[1,1]
 eig = np.linalg.eig(Covmatrix)
 print(eig)
 print('-------')
@@ -162,5 +175,4 @@ plt.plot(dates, princ2,"bo", label = 'Second PC', color = "green")
 plt.xlabel('Years')
 plt.ylabel('Amplitude of principal component')
 plt.legend(numpoints=1,prop={'size':11},loc=3)
-plt.savefig("Princomp.png",dpi=300)
 plt.show()
