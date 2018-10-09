ac282a71549d37e171871d6a853e7c321e4ec1b9
iYorx <42780658+iYorx@users.noreply.github.com>
2018-10-09 11:20:55 +0200

    datetime 59 fix

--- compareDaphne.py
+++ compareDaphne.py
@@ -1,6 +1,19 @@
 
+import numpy as np
+import pandas as pd
+import datetime
 
 dateu = np.load('Ulvebreen/Date.npy')
 daten = np.load("Nordenskioldbreen/Date.npy")
 datel = np.load('Lufthavn2/Date.npy')
 datei = np.load('avgIsfjord/Date.npy')
+
+new=[]
+
+for i in range(10):
+    print(daten[i])
+    pdt= pd.to_datetime(daten[i])
+    if pdt.strftime('%S') != '00':
+        daten[i]=pd.to_datetime(daten[i]) + pd.to_timedelta([1], unit='s')
+        #dtype='datetime64[30m]'
+        
