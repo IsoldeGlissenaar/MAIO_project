ca66016905ddeca787e4645b9992a0694249a89b
IsoldeGlissenaar <43541832+IsoldeGlissenaar@users.noreply.github.com>
2018-10-09 12:47:19 +0200

    save data nordenskiold

--- compareDaphne.py
+++ compareDaphne.py
@@ -5,15 +5,17 @@ import datetime
 
 dateu = np.load('Ulvebreen/Date.npy')
 daten = np.load("Nordenskioldbreen/Date.npy")
-datel = np.load('Lufthavn2/Date.npy')
+datel = np.load('avgLufthavn/Date.npy')
 datei = np.load('avgIsfjord/Date.npy')
 
 new=[]
 
-for i in range(10):
-    print(daten[i])
+for i in range(len(daten)):
     pdt= pd.to_datetime(daten[i])
     if pdt.strftime('%S') != '00':
         daten[i]=pd.to_datetime(daten[i]) + pd.to_timedelta([1], unit='s')
         #dtype='datetime64[30m]'
+
+
+
         
